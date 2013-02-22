4a5cd32bd16579f364c867973b48740ac6301fb0
Karel Bílek <kb@karelbilek.com>
2013-02-22 21:06:09 +0100

    Create correct.pl
    quick and dirty

---
+++ correct.pl
@@ -0,0 +1,24 @@
+$richpoor = $ARGV[0];
+$from = $ARGV[1];
+$to = $ARGV[2];
+
+
+
+#ok, this script is pretty embarassing and very quick and dirty but well I just want to get it done
+system '(
+    echo \'<?xml version="1.0" encoding="utf-8"?>'.
+                            
+                            
+                            #those are hosted on my github page
+'    
+<!DOCTYPE archive PUBLIC "piratebay'.$richpoor.'" "http://runn1ng.github.com/'.$richpoor.'.dtd">
+<archive>\'; 
+cat '.$from.'; 
+echo "</archive>" ) | 
+ perl -pne \'         #I want to escape every & -> &amp;, but not &lt; and &gt; that I escaped before
+    while (/&((([^lg]|$)(.|$)(.|$))|((.|$)([^t]|$)(.|$))|((.|$)(.|$)([^;]|$)))/) {
+        s/&((([^lg]|$)(.|$)(.|$))|((.|$)([^t]|$)(.|$))|((.|$)(.|$)([^;]|$)))/AAAmp;$1/g
+    }; 
+    s/AAAmp/&amp/g\' | 
+        sed \'s/[[:cntrl:]]//g\' > '.$to;
+        #I also want to escape all control chars
